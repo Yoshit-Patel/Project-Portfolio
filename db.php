0d39854d789e675f1e26c5f99e61ce7209022eca
Yoshit Patel <patel.yoshit@gmail.com>
2017-06-27 18:02:50 -0400

    Removing DB credentials to secure database

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";
-$username = "yoshit";
-$password = "patel";
-$dbname = "ai";
+$username = "";
+$password = "";
+$dbname = "";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
